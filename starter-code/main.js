352fc32e5ed8806d6d3a8bcef1e7e32f57453431
Alex Karevoll <alex.karevoll@gmail.com>
2016-08-03 15:27:49 -0700

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,7 @@
 console.log("JS file is connected to HTML! Woo!")
+
+// Sets the values of the four cards for comparing later//
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
