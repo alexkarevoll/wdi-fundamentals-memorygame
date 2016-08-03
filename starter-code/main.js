06343982e4e46e26e1584d6dc97e27b4680b8758
Alex Karevoll <alex.karevoll@gmail.com>
2016-08-03 16:20:55 -0700

    added game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,3 +5,10 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+// Defines game logic //
+if (cardTwo === cardFour){
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again");
+};
