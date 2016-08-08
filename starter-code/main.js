e6de8573db93bd0e3356c07ea81cd6a4f656c1e0
Alex Karevoll <alex.karevoll@gmail.com>
2016-08-08 13:19:06 -0700

    added javascript to create game board

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,6 +1,7 @@
 console.log("JS file is connected to HTML! Woo!")
 
 // Sets the values of the four cards for comparing later//
+/*
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
@@ -12,3 +13,16 @@ if (cardTwo === cardFour){
 } else {
 	alert("Sorry, try again");
 };
+*/
+
+var createBoard = function() {
+	var gameBoard = document.getElementById('game-board');
+
+	for (var i = 0; i <= 4; i++) {
+		var newCard = document.createElement('div');
+		newCard.className = 'card'
+		gameBoard.appendChild(newCard);
+	};
+};
+
+createBoard();
