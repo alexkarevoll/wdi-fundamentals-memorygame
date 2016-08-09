b7ee19a9f2e46c0a886b8009bbbae0ffe4258a80
Alex Karevoll <alex.karevoll@gmail.com>
2016-08-09 16:55:47 -0700

    completed GA requirements, added reset button

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,28 +1,71 @@
 console.log("JS file is connected to HTML! Woo!")
 
-// Sets the values of the four cards for comparing later//
+// Old code to set the values of the four cards for comparing later//
 /*
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
-
-// Defines game logic //
-if (cardTwo === cardFour){
-	alert("You found a match!");
-} else {
-	alert("Sorry, try again");
-};
 */
+// sets possible card attributes
+var cards = ["queen", "queen", "king", "king"];
+// creates an empty set of cards in play to be filled later
+var cardsInPlay = [];
 
+//function to create a game board
 var createBoard = function() {
 	var gameBoard = document.getElementById('game-board');
-
-	for (var i = 0; i <= 4; i++) {
+//creates cards in game board
+	for (var i = 0; i < cards.length; i++) {
 		var newCard = document.createElement('div');
-		newCard.className = 'card'
+		//links the cards link to the css
+		newCard.className = 'card';
+		//gives cards attributes of queen and king
+		newCard.setAttribute ('data-card', cards[i]);
+		//creates listener on click to check if we need to look for
+		//a match because a second card has been clicked
+		newCard.addEventListener('click', isTwoCards);
+		// adds new card to game board
 		gameBoard.appendChild(newCard);
 	};
 };
 
+// Defines game logic for matching 
+// currently matches if same card is clicked twice
+var isMatch = function(){
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
+		} 
+		else {
+		alert("Sorry, try again");
+	};
+};
+
+//puts cards in play. when there are two, runs isMatch
+var isTwoCards = function(){
+	//puts a card in cardsInPlay with its king/queen attribute
+	cardsInPlay.push(this.getAttribute('data-card'));
+	//flips the card to an image based on its attribute
+	if(this.getAttribute('data-card') === "king"){
+		this.innerHTML = '<img src="king.png" alt="King" />';
+	}
+	else if (this.getAttribute('data-card') === "queen"){
+		this.innerHTML = '<img src="queen.jpg" alt="Queen" />';
+	}
+	// if there are two cards look for a match
+	if (cardsInPlay.length === 2) {
+		//puts cardsInPlay into isMatch
+		isMatch(cardsInPlay);
+		//clears the board
+		cardsInPlay = [];
+	}
+};
+
+// calls function and makes board
 createBoard();
+
+//logic for reset button
+//currently just refreshes the page
+var resetBoard = function (){
+	location.reload();
+};
